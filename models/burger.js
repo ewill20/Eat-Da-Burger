39879a19f46c9f0944facd6c047982b29fafbe07
ewill20 <ewill2020@gmail.com>
2017-10-06 14:22:28 -0500

    models finished

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burgers = {
+    all: function(cb) {
+        orm.all('burgers', col, vals, functions(res) {
+            cb(res);
+        });
+    },
+    create: function(col, vals, cb) {
+        orm.create('burgers', col, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(col, vals, cb) {
+        orm.update('burgers', objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burgers;
